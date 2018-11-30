b15eabf567a5224d310f77a38127152a60ca33a8
varleinth <45482494+varleinth@users.noreply.github.com>
2018-11-30 16:20:51 +0700

    Add files via upload

---
+++ UserData.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BluejackMarket;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Humanoidss
+ */
+public class UserData {
+     public static Vector<String> username = new Vector<>();
+     public static Vector<String> passwords = new Vector<>();
+}
